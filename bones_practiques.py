db9b434079d63b87ba584880105591abb993003a
hengchen16 <hchen@instituticaria.cat>
2025-10-22 11:03:39 +0200

    si

---
+++ bones_practiques.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+'''bones_practiques.py. Calcula una divisió i et dona el quocient i residu
+
+Institut Icària
+
+Programació - 1r Batxillerat - Curs 2025-36
+
+El programa et demana un dividend i un divisor per després poder calcualar
+el quocient i el residu.
+'''
+
+__author__ = "Heng Chen"
+__email__="hchen@instituticaria.cat"
+__date__ = "2025/10/22"
+
+
+dividendo = int(input("Posa el dividend: "))
+divisor = int(input("Posa el divisor: "))
+
+cociente = dividendo // divisor
+residuo = dividendo % divisor 
+
+print(f"Divisió: {dividendo}/{divisor}")
+print(f"Quocient: {cociente}")
+print(f"Residu: {residuo}") 
